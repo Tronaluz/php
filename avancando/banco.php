476e7f7ded857d40abe780f0bf9f10d57522765f
Lucas <lucas_tron@hotmail.com>
2022-05-03 12:28:17 -0300

    update 2

--- avancando/banco.php
+++ avancando/banco.php
@@ -1,5 +1,9 @@
 <?php
 
+function exibeMensagem ($mensagem) {
+    echo $mensagem . PHP_EOL;
+}
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Vinicius',
@@ -15,22 +19,18 @@ $contasCorrentes = [
     ],
 ];
 
-$contasCorrentes['173.496.711-90'] = [
-    'titular' => 'Claudia',
-    'saldo' => 2000,
-];
 
 if (500 > $contasCorrentes['123.666.789-10']['saldo']) {
-    echo "Você não pode sacar";
+    exibeMensagem (mensagem:"Você não pode sacar");
 } else {
     $contasCorrentes['123.666.789-10']['saldo'] -= 500;
 }
 
 if (500 > $contasCorrentes['123.456.711-10']['saldo']) {
-    echo "Você não pode sacar este valor" . PHP_EOL;
+    exibeMensagem (mensagem:"Você não pode sacar");
 } else { $contasCorrentes['123.456.711-10']['saldo'] -= 500;
 }
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] . PHP_EOL;
+    exibeMensagem( $cpf . " " . $conta['titular'] . ' ' . $conta['saldo']);
 }
